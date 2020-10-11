4a33a418df520f19dc4fa2176d19ea153349c7e5
Zeztron <pateltharsh@gmail.com>
2020-10-11 18:18:41 -0400

    added readme

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express';
 
 const app: Application = express();
-const PORT = 3001;
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
