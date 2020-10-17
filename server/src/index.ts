6f0182df045d9ecbc3c035b47847fcd0ef515f3a
Zeztron <pateltharsh@gmail.com>
2020-10-17 10:34:53 -0400

    set up react router

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,5 +5,4 @@ dotenv.config();
 
 const app: Application = express();
 
-const PORT: number = 5000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${process.env.PORT}`));
